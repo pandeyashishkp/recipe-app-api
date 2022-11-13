6a0460c72046f6bea4798ddae9f2531458ef95fc
pandeyashishkp <pandey.ashishkp@gmail.com>
2022-11-13 12:42:17 +0530

    Linting resolution

--- app/core/management/commands/wait_for_db.py
+++ app/core/management/commands/wait_for_db.py
@@ -6,13 +6,13 @@ import time
 from psycopg2 import OperationalError as Psycopg2OpError
 from django.db.utils import OperationalError
 
-
 from django.core.management.base import BaseCommand
+
 
 class Command(BaseCommand):
     """ Django command to wait for database."""
 
-    def handle(self,*args,**options):
+    def handle(self, *args, **options):
         """Entrypoint for command."""
         self.stdout.write("Waiting for database...")
         db_up = False
@@ -20,7 +20,7 @@ class Command(BaseCommand):
             try:
                 self.check(databases=['default'])
                 db_up = True
-            except (Psycopg2OpError,OperationalError):
+            except (Psycopg2OpError, OperationalError):
                 self.stdout.write('Database unavailable, waiting 1 second...')
                 time.sleep(1)
 
--- app/core/tests/test_commands.py
+++ app/core/tests/test_commands.py
@@ -14,13 +14,12 @@ from django.test import SimpleTestCase
 class CommandTests(SimpleTestCase):
     """Test Commands"""
 
-    def test_wait_for_db_ready(self,patched_check):
+    def test_wait_for_db_ready(self, patched_check):
         patched_check.return_value = True
 
         call_command('wait_for_db')
 
         patched_check.assert_called_once_with(databases=['default'])
-
 
     @patch('time.sleep')
     def test_wait_for_db_delay(self, patched_sleep, patched_check):
@@ -30,5 +29,5 @@ class CommandTests(SimpleTestCase):
 
         call_command('wait_for_db')
 
-        self.assertEqual(patched_check.call_count,6)
+        self.assertEqual(patched_check.call_count, 6)
         patched_check.assert_called_with(databases=['default'])
